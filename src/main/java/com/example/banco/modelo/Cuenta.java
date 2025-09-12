5dbb84b9098c32db1c1d97566b51cf95dbf501d2
yeferlol12 <ysantamaria@cesde.net>
2025-09-12 09:13:38 -0500

    actualizacion del

--- src/main/java/com/example/banco/modelo/Cuenta.java
+++ src/main/java/com/example/banco/modelo/Cuenta.java
@@ -1,9 +1,12 @@
 package com.example.banco.modelo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +20,15 @@ public class Cuenta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
-    private int cuenta;
+    
+    @Column(unique = true, nullable = false)
+    private int numeroCuenta;
+    
+    @Column(nullable = false)
+    private double saldo;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
 }
